601255e673022664a7f769f151590ab224d3ac60
Nic Westvold <nic.westvold@grafana.com>
2026-05-04 18:29:49 -0400

    feat(query-history): add queryHistoryRecentQueriesUIflag andRecentQueriesContext service layer

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -129,6 +129,11 @@ export interface FeatureToggles {
   */
   queryHistoryLocalOnly?: boolean;
   /**
+  * Replace the Query History drawer with a new Recent Queries modal UI
+  * @default false
+  */
+  queryHistoryRecentQueriesUI?: boolean;
+  /**
   * Support temporary security credentials in AWS plugins for Grafana Cloud customers
   * @default true
   */
